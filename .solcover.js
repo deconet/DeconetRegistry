a83b74032ab3da17d9a21603179aca9a6f8b79db
Shachindra <shachindra.spidey@gmail.com>
2018-06-14 22:35:21 +0530

    Worked on test cases

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,8 @@
 module.exports = {
   // use the local version of truffle
-  testCommand: '../node_modules/.bin/truffle test --network coverage',
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+
+  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
   // start blockchain on the same port specified in truffle.js
   // use the default delicious Ganache mnemonic
   testrpcOptions: '-p 7545 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"'
