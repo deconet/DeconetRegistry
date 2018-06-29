36dfd85cb8141a4de147a9a6bb8d0ffabddd0cf9
Chris Cassano <glitch0@gmail.com>
2018-06-29 13:09:23 -0700

    got tests running locally

--- truffle.js
+++ truffle.js
@@ -15,6 +15,11 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
     mainnet: {
       provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/NGCFzmhFCfPHVp5h4Umf'),
       network_id: '1',
