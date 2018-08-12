f5cea18a880a843ea1078c77d48ad0e5261da239
Shachindra <shachindra.spidey@gmail.com>
2018-08-12 18:52:02 +0530

    Removed conflicting files

--- .solcover.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // use the local version of truffle
-  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-
-  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-  // start blockchain on the same port specified in truffle.js
-  // use the default delicious Ganache mnemonic
-  testrpcOptions: '-p 7545 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"'
-};
